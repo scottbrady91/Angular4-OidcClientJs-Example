dee0f012d11d1cd7e07eeec6acb33e2f232aa1d1
Scott Brady <scott.brady@rocksolidknowledge.com>
2017-04-21 12:23:28 +0100

    Added component with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ProtectedComponent } from './protected/protected.component';
+
 const routes: Routes = [
   {
     path: '',
     children: []
+  },
+  {
+    path: 'protected',
+    component: ProtectedComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProtectedComponent } from './protected/protected.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProtectedComponent
   ],
   imports: [
     BrowserModule,
